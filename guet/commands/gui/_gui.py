5492920ec53864200078703c0fb82758864b9e4d
Rajat <emailofrajatyadav@gmail.com>
2022-02-11 20:57:32 -0700

    GUI command line integrated TG-7 #done

--- guet/commands/gui/_gui.py
+++ guet/commands/gui/_gui.py
@@ -1,2 +1,19 @@
-class GuiCommandFactory():
-    exec(open("execute.py").read()) 
+from guet.commands import CommandFactory
+from guet.steps import Step
+from guet.steps.check import (CommittersExistCheck, GitRequiredCheck,
+                              HelpCheck, VersionCheck)
+from guet.steps.preparation import InitializePreparation, SwapToLocal
+from guet.util import HelpMessageBuilder
+from  ._setGUI import SetGUI
+
+class GuiCommandFactory(CommandFactory):
+    def __init__(self, file_system, committers, current, git):
+        self.committers = committers
+        self.current = current
+        self.file_system = file_system
+        self.git = git
+
+    def build(self) -> Step:
+        return VersionCheck() \
+            .next(SetGUI()) 
+            #.next(CommittersExistCheck(self.committers)) \

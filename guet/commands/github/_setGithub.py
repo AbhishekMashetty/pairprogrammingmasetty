ab01d1f4f345b713ecda560cb27b43ff970dcb6d
“Abhishek” <amasetty@asu.edu>
2022-02-16 22:16:40 -0700

    TG-37 #ready-for-test fixed the issue of printing the output

--- guet/commands/github/_setGithub.py
+++ guet/commands/github/_setGithub.py
@@ -7,7 +7,7 @@ from guet.steps.action import Action
 class SetGithub(Action):
     token = os.getenv('GITHUB_TOKEN', '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')
     owner = "AbhishekMashetty"
-    repo = "pairprogrammingmasetty"
+    repo = "assign-git"
     query_url = f"https://api.github.com/repos/{owner}/{repo}/issues"
     params = {
     "state": "open",
@@ -18,9 +18,11 @@ class SetGithub(Action):
 
     def execute(self, args: List[str]):
         if args[0]=="issues":
-            self.getdata()
+            print("Entered the execute")
+            print(self.getdata())
 
     def getdata(self):
+        print("Entered the getdata")
         resp = requests.get(SetGithub.query_url, headers=SetGithub.headers, params=SetGithub.params)
         output = resp.json()
         output_map = {}

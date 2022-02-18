c08fc72f6b1e0925ffc83336e3381f5e10d83232
kartavya-asu <90439756+kartavya-asu@users.noreply.github.com>
2022-02-18 16:41:12 -0700

    Add data to session tracking file

--- guet/commands/session/_sessionTracker.py
+++ guet/commands/session/_sessionTracker.py
@@ -1,8 +1,17 @@
+import time
+from os.path import join
+from pathlib import Path
 from typing import List
+from datetime import datetime, timedelta
 
+from guet import constants
 from guet.committers import Committers2 as Committers
 from guet.committers import CommittersPrinter, CurrentCommitters
 from guet.steps.action import Action
+from guet.config import CONFIGURATION_DIRECTORY
+from guet.files.write_lines import write_lines
+from guet.files import FileSystem
+from guet.util import project_root
 
 
 class SessionTrackerAction(Action):
@@ -13,16 +22,45 @@ class SessionTrackerAction(Action):
         self.committers = committers
         self.current_committers = current_committers
 
+    # def new_timestamp():
+    #     # Creates a new timestamp with a specific format.
+    #     return datetime.now().strftime("%d/%m/%y - %H:%M:%S")
+
     def execute(self, args: List[str]):
 
         if args[0] == 'start':
             print(":::Pair-Programming Session Starts:::")
+            path = Path(join(CONFIGURATION_DIRECTORY, constants.SESSION_TRACKER))
+            project_path = project_root()
+            timeStamp = datetime.now().strftime("%d-%m-%y-%H:%M:%S")
+            sessionDetails = timeStamp + "," + f'{project_path}\n'
+            sessionExist = False
+            print(project_path)
+            if path.is_file():
+                with open(path) as sessionFile:
+                    sessionRecords = sessionFile.readlines()
+                for session in sessionRecords:
+                    if project_path == session.split(',')[-1]:
+                        sessionExist = True
+                        print("Session Already Exist")
+            if not sessionExist:    
+                with path.open('a') as fp:
+                    sessionExist = True
+                    fp.write(sessionDetails)
+            #path.write_text(sessionDetails)
+            #write_lines(path, timeStamp + "," + f'{project_path}')
             # committers = self.committers.all()
             # pre_print = 'All committers'
-        else:
+        elif args[0] == 'end':
             print(":::Pair-Programming Session Ends:::")
             # committers = self.current.get()
             # pre_print = 'Current committers'
+        else:
+            print("Please pass a valid <identifier>")
+            print("For details & help:")
+            print("Use: guet session --help")
+
+    
         
 
 

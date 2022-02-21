3447ab3b86b6494764e0b16bd9b7fd5a9e06769d
vsingh57 <vsingh57@asu.edu>
2022-02-20 20:01:06 -0700

    TG-56 Fixed naigation frame

--- guet/UI.py
+++ guet/UI.py
@@ -1,6 +1,8 @@
+from ctypes import alignment
 from tkinter import *
 from guet.commands import CommandMap
 from guet.files import FileSystem
+import os
 
 
 
@@ -11,38 +13,55 @@ class GUI():
         self.fileSystem = FileSystem()
 
     def execute(self):
-        root = Tk()
-        root.geometry('2000x2000')
+        self.root = Tk()
+        self.root.title('GUET')
+        self.root.geometry('500x400')
         #Heading text
-        myLabel = Label(root, text="Welcome to guet")
-        label2 = Label(root, text="Do pair programming with ease")
+        #myLabel = Label(self.root, text="Welcome to guet")
+        #label2 = Label(self.root, text="Do pair programming with ease")
 
-        myLabel.grid(row=0, column=0)
-        label2.grid(row=2, column=0)
+        #myLabel.grid(row=0, column=0)
+        #label2.grid(row=2, column=0)
+
+        self.nav = Frame(self.root, bg='red')
+        self.nav.pack(side=LEFT)
+
+        self.view = Frame(self.root, bg='blue')
+        self.view.pack(side=RIGHT)
 
 
-        #Buttons
+        #Nav Buttons
+        button1 = Button(self.root, text="Init", command=self.guetInit, height=2, width=10)
+        #button1.grid(row=5, column=0)
 
-        button1 = Button(root, text="Init", command=self.guetGUI)
-        button1.grid(row=5, column=0)
+        button2 = Button(self.root, text="Yeet", command=self.guetYeet, height=2, width=10)
+        #button2.grid(row=10, column=0)
 
-        button2 = Button(root, text="Yeet", command=self.guetYeet)
-        button2.grid(row=6, column=0)
+        button3 = Button(self.root, text="Add", height=2, width=10, command=self.showAdd)
+        #button3.grid(row=6, column=0)
 
-        button3 = Button(root, text="Button 3")
-        button3.grid(row=7, column=0)
+        button4 = Button(self.root, text="Get", height=2, width=10)
+        #button4.grid(row=7, column=0)
 
-        button4 = Button(root, text="Button 4")
-        button4.grid(row=8, column=0)
+        button5 = Button(self.root, text="Set", height=2, width=10)
+        #button5.grid(row=8, column=0)
 
-        button5 = Button(root, text="Button 5")
-        button5.grid(row=9, column=0)
+        button6 = Button(self.root, text="Remove", height=2, width=10)
+        #button6.grid(row=9, column=0)
 
-        button6 = Button(root, text="Button 6")
-        button6.grid(row=10, column=0)
+        label1 = Label(self.view, text='Repository: ' + os.getcwd(), anchor='w')
+        label1.pack(side=TOP)
+        #label1.grid(row =0, column=30)
+
+        button1.pack(side=TOP, anchor='w')
+        button2.pack(side=TOP, anchor='w')
+        button3.pack(side=TOP, anchor='w')
+        button4.pack(side=TOP, anchor='w')
+        button5.pack(side=TOP, anchor='w')
+        button6.pack(side=TOP, anchor='w')
 
         #Main loop
-        root.mainloop()
+        self.root.mainloop()
 
     
     def guetYeet(self):
@@ -51,8 +70,15 @@ class GUI():
 
         self.fileSystem.save_all()
     
-    def guetGUI(self):
+    def guetInit(self):
         command = self.commandMap.get_command('init').build()
         command.play([])
 
         self.fileSystem.save_all()
+
+
+    def showAdd(self):
+        for widget in self.view.winfo_children():
+            widget.destroy()
+        newLabel= Label(self.view, text='Add frame')
+        newLabel.pack()

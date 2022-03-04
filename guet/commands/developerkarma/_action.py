510333cacf4b61715d95434742625ec0a355f628
kartavya <kchauha6@asu.edu>
2022-03-04 00:53:20 -0700

    TG-77 Fetch and process data from Git logs

--- guet/commands/developerkarma/_action.py
+++ guet/commands/developerkarma/_action.py
@@ -1,16 +1,15 @@
-import time
-from os.path import join
+from itertools import count
+import subprocess
+import sys
+
 from pathlib import Path
 from typing import List
-from datetime import datetime, timedelta
+from collections import Counter
 
 from guet import constants
 from guet.committers import Committers2 as Committers
 from guet.committers import CommittersPrinter, CurrentCommitters
 from guet.steps.action import Action
-from guet.config import CONFIGURATION_DIRECTORY
-from guet.files.write_lines import write_lines
-from guet.files import FileSystem
 from guet.util import project_root
 
 
@@ -23,5 +22,27 @@ class DeveloperKarmaAction(Action):
         self.current_committers = current_committers
 
     def execute(self, args: List[str]):
+        package = 'GitPython'
+        import importlib
+        try:
+            importlib.import_module('git')
+            print(package + '- Package already exists')
+        except ImportError:
+            subprocess.check_call([sys.executable,'-m','pip','install',package])
+        finally:
+            globals()[package] = importlib.import_module('git')
+            print('git ' + ' Package Imported')
+
+        import git
         print("#####Developer Karma Display#####")
-        
+        path = Path(str(project_root()))
+        repo = git.Repo(path)
+        commits = list(repo.iter_commits("main"))
+        committers = []
+        for commit in commits:
+            committers.append(commit.author)
+        developerKarma = Counter(committers)
+        print("Aggregated Contributions from each committer to main branch")
+        print("Committer:Commits")
+        for sign, count in developerKarma.most_common():
+            print(str(sign) + ":" + str(count))
